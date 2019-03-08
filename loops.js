7ff73b6a4571dd071642ca8d9dfcb44262622bb9
Gino Desangles <blushingbox@yahoo.com>
2019-03-08 01:51:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,5 +18,15 @@ function whileLoop (num) {
 }
 
 function doWhileLoop (num) {
+  var i = 0;
+ 
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   
+  do {
+    console.log("I run once regardless.")
+  }
+  while (incrementVariable() < num)
 }
