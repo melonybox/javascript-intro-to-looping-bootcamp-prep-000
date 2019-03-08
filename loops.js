a9b1cfc2ee925bdf02f38cdfe346e93dc75b250e
Gino Desangles <blushingbox@yahoo.com>
2019-03-08 01:31:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
-    if ( i === 0 ) {
-      array.push("I am 1 strange loop.")
+    if ( i < 25 ) {
+      array.push("I am ${i} strange loop.")
     }
-      else {
-        return "I am ${i} strange loops."
+      else ( i === 0 ) {
+        array"I am ${i} strange loops."
     }
   }
 }
