be66d811dd902dc9aeae332d1c4e28c626290864
Gino Desangles <blushingbox@yahoo.com>
2019-03-08 01:40:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,8 +3,8 @@ function forLoop (array) {
     if ( i === 1 ) {
       array.push("I am 1 strange loop.")
     }
-      else {
+      else
         array.push("I am ${i} strange loops.")
-    }
   }
+  return array
 }
